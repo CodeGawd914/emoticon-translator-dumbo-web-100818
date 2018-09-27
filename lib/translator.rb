1c20035e2989a389a8a32cedabf521d39ae0cb5e
Travis Desir <travisgdesir31@gmail.com>
2018-09-27 04:01:06 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   list = YAML.load_file(file_path)
-  list.each do |
+  list.each do |meaning,value|
   binding.pry
+end
 end
 
 def get_english_meaning
